a0c9b5f2856a537c980aa7a9aac8c4c3729902f8
Jieun35 <90733575+Jieun35@users.noreply.github.com>
2021-09-30 11:27:08 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,19 +1,22 @@
+/**
+ * 210930 무선침입자시스템
+ */
+radio.onReceivedNumber(function (receivedNumber) {
+    pins.digitalWritePin(DigitalPin.P1, receivedNumber * 경비전원)
+    music.setVolume(receivedNumber * 경비전원)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
 input.onButtonPressed(Button.A, function () {
-	
+    경비전원 = 1
 })
 input.onButtonPressed(Button.B, function () {
-	
+    경비전원 = 0
 })
+let 경비전원 = 0
 radio.setGroup(5)
 basic.forever(function () {
-    if (tinkercademy.PIR(DigitalPin.P0)) {
-        radio.sendNumber(1)
-    } else {
-        radio.sendNumber(2)
-    }
-    if (tinkercademy.PIR(DigitalPin.P1)) {
-        radio.sendNumber(1)
-    } else {
-        radio.sendNumber(2)
-    }
+	
+})
+basic.forever(function () {
+    radio.sendNumber(pins.digitalReadPin(DigitalPin.P0))
 })
